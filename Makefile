b17ce30936e86079fd223844353fb041d1377689
boreddad <ronnebaumfinances@gmail.com>
2023-12-26 17:43:14 -0500

    feat: bring in tailwind

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 all:
 	go build -o bin/circles
+
+css:
+	pnpm css
+
+dev:
+	air & pnpm dev
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
