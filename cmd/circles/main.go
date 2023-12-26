a07818fffb45affab462b864c08acc27fc0ff77e
boreddad <ronnebaumfinances@gmail.com>
2023-12-26 17:34:55 -0500

    feat: hello world

--- cmd/circles/main.go
+++ cmd/circles/main.go
@@ -1,7 +1,26 @@
 package circles
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/vincer2040/circles/internal/render"
+)
 
 func Main() {
-    fmt.Println("hi mom")
+
+	e := echo.New()
+
+	e.Renderer = render.New()
+
+	e.Use(middleware.Logger())
+
+	e.GET("/", rootGet)
+
+	e.Logger.Fatal(e.Start(":6969"))
+}
+
+func rootGet(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", nil)
 }
