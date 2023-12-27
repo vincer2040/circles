daa63f1472344cec0ddc9f51229b82d6466de343
boreddad <ronnebaumfinances@gmail.com>
2023-12-26 21:07:42 -0500

    feat: render different navbar based on the route

--- internal/routes/root.go
+++ internal/routes/root.go
@@ -4,8 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/vincer2040/circles/internal/util"
 )
 
 func RootGet(c echo.Context) error {
-    return c.Render(http.StatusOK, "index.html", nil)
+    pageData := util.PageData {
+        Route: "/",
+    }
+    return c.Render(http.StatusOK, "index.html", pageData)
 }
