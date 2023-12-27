f7e0d06424e43a71c044f37716b992d43e4b9ed5
boreddad <ronnebaumfinances@gmail.com>
2023-12-26 21:01:57 -0500

    feat: user type

---
+++ internal/user/user.go
@@ -0,0 +1,25 @@
+package user
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	First    string
+	Last     string
+	Email    string
+	Password string
+}
+
+func New(first string, last string, email string, password string) User {
+	user := User{first, last, email, password}
+	user.HashPassword()
+	return user
+}
+
+func (user *User) HashPassword() error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
